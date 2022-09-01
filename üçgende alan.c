0ec4549034f68487266bc411bda4677f882c171a
redrosecode <112544023+redrosecode@users.noreply.github.com>
2022-09-01 17:21:00 +0300

    Add files via upload

---
+++ üçgende alan.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	int a,h;
+	float alan;
+	printf("Kenaar uzunugunu giriniz:");
+	scanf("%d",&a);
+	printf("Kenara ait yuksekligi giriniz:");
+	scanf("%d",&h);
+	alan=a*h/2;
+	printf("alan=%f",alan);
+	getch();
+}
